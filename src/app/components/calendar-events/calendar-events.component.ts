b72974b291aa57542d86041d382add97081a8f3f
Jonayet <jonayet.bu@gmail.com>
2016-12-18 02:36:52 +0600

    updated

--- src/app/components/calendar-events/calendar-events.component.ts
+++ src/app/components/calendar-events/calendar-events.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, ViewChild} from '@angular/core';
 import {ICalendarEvent} from '../calendar/ICalendarEvent';
 import {ICalendarDay} from '../calendar/ICalendarDay';
+import {CalendarAddEventComponent} from '../calendar-add-event/calendar-add-event.component';
 
 @Component({
     selector: 'calendar-events',
@@ -11,12 +12,14 @@ export class CalendarEventsComponent {
     @Input() day: ICalendarDay;
     @Input() events: ICalendarEvent[];
     @Output() onAddEvent = new EventEmitter<ICalendarDay>();
+    @ViewChild(CalendarAddEventComponent) addEventComponent: CalendarAddEventComponent;
 
     constructor() {
 
     }
 
     addClicked(){
+        this.addEventComponent.open();
         this.onAddEvent.emit(this.day);
     }
 }
